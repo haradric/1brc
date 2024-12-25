b9eb21ea426f7e03a527dfa2b6e6ece965662999
Mykyta Braslavskyi <nikita.braslavskiy@gmail.com>
2024-12-25 05:00:57 +0200

    optimized statistics calculation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,37 @@ use itertools::Itertools;
 
 const FILENAME: &str = "measurements_1b.txt";
 
+struct Stats {
+    min: f32,
+    max: f32,
+    sum: f32,
+    count: usize,
+}
+
+impl Stats {
+    const fn new() -> Self {
+        Self {
+            min: f32::INFINITY,
+            max: f32::NEG_INFINITY,
+            sum: 0.0,
+            count: 0,
+        }
+    }
+
+    fn add(&mut self, value: f32) {
+        self.min = self.min.min(value);
+        self.max = self.max.max(value);
+        self.sum += value;
+        self.count += 1;
+    }
+
+    fn finalize(self) -> (f32, f32, f32) {
+        #[allow(clippy::cast_precision_loss)]
+        let avg = self.sum / self.count as f32;
+        (self.min, avg, self.max)
+    }
+}
+
 fn main() {
     let mut measurements = HashMap::new();
 
@@ -11,36 +42,25 @@ fn main() {
         .expect("Failed to read the file")
         .lines()
         .for_each(|line| {
-            let mut parts = line.split_terminator(';');
-            let station = parts.next().expect("Invalid line format");
-            let temperature = parts
-                .next()
-                .unwrap()
-                .parse::<f64>()
-                .expect("Invalid value format");
+            let (station, value) = line.split_once(';').expect("Invalid line format");
+            let value = value.parse::<f32>().expect("Invalid value format");
 
             measurements
                 .entry(station.to_owned())
-                .or_insert_with(Vec::new)
-                .push(temperature);
+                .and_modify(|entry: &mut Stats| {
+                    entry.add(value);
+                })
+                .or_insert_with(|| {
+                    let mut entry = Stats::new();
+                    entry.add(value);
+                    entry
+                });
         });
 
     let results = measurements
         .into_iter()
-        .map(|(station, temps)| {
-            let min = *temps
-                .iter()
-                .min_by(|a, b| a.partial_cmp(b).unwrap())
-                .unwrap();
-            let avg = temps.iter().sum::<f64>() / temps.len() as f64;
-            let max = *temps
-                .iter()
-                .max_by(|a, b| a.partial_cmp(b).unwrap())
-                .unwrap();
-
-            (station, (min, avg, max))
-        })
-        .collect::<BTreeMap<String, (f64, f64, f64)>>();
+        .map(|(station, stats)| (station, stats.finalize()))
+        .collect::<BTreeMap<String, (f32, f32, f32)>>();
 
     print!("{{");
     #[allow(unstable_name_collisions)]
